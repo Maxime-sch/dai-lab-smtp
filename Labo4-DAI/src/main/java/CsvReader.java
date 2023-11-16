82067dbb94837fabd485759ab53d7be7cb1fd98c
tomas-pavoni <tomas.pavoni@heig-vd.ch>
2023-11-16 14:32:32 +0100

    CsvReader readVictim

--- Labo4-DAI/src/main/java/CsvReader.java
+++ Labo4-DAI/src/main/java/CsvReader.java
@@ -1,2 +1,24 @@
+import java.io.*;
+import java.util.*;
+
 public class CsvReader {
+    private final String delimiter;
+
+    CsvReader(String delimiter) {
+        this.delimiter = delimiter;
+    }
+    public ArrayList<String> readVictim(File file) {
+        ArrayList<String> victims = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(delimiter);
+                victims.addAll(Arrays.asList(values));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        return victims;
+    }
 }
