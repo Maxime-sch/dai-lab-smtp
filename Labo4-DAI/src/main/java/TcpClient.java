855f03fe616ebe72f42745420c4ed6af72efc7b1
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-11-30 14:34:41 +0100

    TcpClient basic implementation works

--- Labo4-DAI/src/main/java/TcpClient.java
+++ Labo4-DAI/src/main/java/TcpClient.java
@@ -5,30 +5,31 @@ import java.util.List;
 public class TcpClient {
     public static void main(String[] args) {
 
-        CsvReader csvReader = new CsvReader(",");
+        CsvReader csvReaderVirgule = new CsvReader(",");
+        CsvReader csvReaderPtVirgule = new CsvReader(";");
 
         String victimsFilePath = args[0];
         String messagesFileName = args[1];
         int groupsAmount = Integer.parseInt(args[2]);
 
         //read victims, messages and put victims in List<String>
-        List<String> addresses = csvReader.readVictim(new File(victimsFilePath));
-        List<String> messages = new ArrayList<>(); // csvReader.readMessages(new File(victimsFilePath));
+        List<String> addresses = csvReaderVirgule.readVictim(new File(victimsFilePath));
+        List<String>[] messages = csvReaderPtVirgule.readMessage(new File(messagesFileName));
         List<List<String>> groups = GroupMaker.group(addresses, groupsAmount);
 
         List<String> sendTo;
         String from;
 
         //Test, TODO REMOVE
-        messages.add("Message de test.");
 
         for (int i = 0; i < groups.size(); ++i){
             sendTo = groups.get(i);
             from = sendTo.getFirst();
             sendTo.removeFirst(); //first or last??
 
-            String message = messages.get(0); //TODO accéder au bon message
-            SmtpClient.sendEmail(from, from, sendTo, message, message);
+            String subject = messages[0].get(i); //TODO accéder au bon message
+            String body = messages[1].get(i);
+            SmtpClient.sendEmail(from, from, sendTo, subject, body);
         }
         //TODO gestion edge cases
     }
