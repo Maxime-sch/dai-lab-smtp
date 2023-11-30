a2997c6630616b46f01886a399605d8081b3b17e
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-11-30 14:27:25 +0100

    Continue implementation of TcpClient

--- Labo4-DAI/src/main/java/TcpClient.java
+++ Labo4-DAI/src/main/java/TcpClient.java
@@ -1,26 +1,25 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TcpClient {
     public static void main(String[] args) {
 
-        //String victimsFileName = args[0];
-        //String messagesFileName = args[1];
-        //int groupsAmount = Integer.parseInt(args[2]);
+        CsvReader csvReader = new CsvReader(",");
+
+        String victimsFilePath = args[0];
+        String messagesFileName = args[1];
+        int groupsAmount = Integer.parseInt(args[2]);
 
         //read victims, messages and put victims in List<String>
-        List<String> addresses = new ArrayList<>();
-        List<String> messages = new ArrayList<>();
-        List<List<String>> groups = new ArrayList<>();//GroupMaker.group(addresses, groupsAmount);
+        List<String> addresses = csvReader.readVictim(new File(victimsFilePath));
+        List<String> messages = new ArrayList<>(); // csvReader.readMessages(new File(victimsFilePath));
+        List<List<String>> groups = GroupMaker.group(addresses, groupsAmount);
 
         List<String> sendTo;
         String from;
 
-        //Test, TODO remove
-        List<String> group1 = new ArrayList<>();
-        group1.add("envoyeur@mail.ch");
-        group1.add("receveur@mail.ch");
-        groups.add(group1);
+        //Test, TODO REMOVE
         messages.add("Message de test.");
 
         for (int i = 0; i < groups.size(); ++i){
