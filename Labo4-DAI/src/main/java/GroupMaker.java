5b19b4103f90b0d8f28aadc4696abb6cce08b8a2
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-12-03 14:35:39 +0100

    Finished GroupMaker

--- Labo4-DAI/src/main/java/GroupMaker.java
+++ Labo4-DAI/src/main/java/GroupMaker.java
@@ -2,10 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 import static java.lang.Math.round;
 
+/**
+ * This class provides a static method to group email addresses.
+ */
 public class GroupMaker {
+    /**
+     * Method to group a list of email addresses into n groups, with a maximum of 5 and a minimum of 2 email addresses
+     * per group
+     * @param addresses the email addresses to group, given as a List of Strings
+     * @param n the amount of groups to make
+     * @return a List of Lists, each second-level list being a group of email addresses
+     * @throws RuntimeException if there are too many or too little addresses to have a group size <5 and >2
+     */
     static List<List<String>> group(List<String> addresses, int n){
-        int totalAddresses = addresses.size();
-        float avgGrpSize = (float)(totalAddresses) / (float) (n);
+        float totalAddresses = addresses.size();
+        float avgGrpSize = totalAddresses / (float) (n);
         if(avgGrpSize < 2){
             throw new RuntimeException("There are too many groups to put at least 2 addresses per group");
         }
@@ -14,34 +25,21 @@ public class GroupMaker {
         }
 
         List<List<String>> result = new ArrayList<>();
-
         int curIndex = 0;
         int curGrpSize;
 
         for(int i = 0; i < n; ++i){
-            result.add(new ArrayList<>()); //ajouter un groupe vide
+            result.add(new ArrayList<>()); //add new empty group
 
-            avgGrpSize = (float)(totalAddresses - curIndex) / (float) (n - i);
+            //update average group size to choose how many addresses to put in the current group
+            avgGrpSize = (totalAddresses - curIndex) / (float) (n - i);
             curGrpSize = round(avgGrpSize);
 
-            for(int j = 0; j < curGrpSize; ++j){
-                result.get(i).add(addresses.get(curIndex)); //remplir le groupe courant
+            for(int j = 0; j < curGrpSize; ++j){ //fill the current group
+                result.get(i).add(addresses.get(curIndex));
                 ++curIndex;
             }
         }
         return result;
-    }
-
-    public static void main(String[] args){ //Programme de test de groupage simple
-        List<String> testArrayList = new ArrayList<>();
-        for(int i=0; i<20; i++){
-            testArrayList.add("Person" + Integer.toString(i) + "@gmail.com");
-        }
-
-        List<List<String>> testGrp = group(testArrayList, 4);
-
-        for(List<String> i : testGrp){
-            System.out.println(i);
-        }
     }
 }
